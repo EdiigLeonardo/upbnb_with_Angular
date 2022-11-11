b72977c5e7db023e555ef961fcf233c48d0faa02
unknown <ediigmelchiior@gmail.com>
2022-11-11 10:51:16 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'projecto_-_M9_-_edig_-_leonardo';
+}
